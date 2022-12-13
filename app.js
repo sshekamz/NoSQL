561d24526bc42d856a7ab9e0b983eccd647fb9d7
sshekamz <shashanknkework@gmail.com>
2022-12-14 00:01:37 +0530

    User

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const User=require('./models/user');
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
 
@@ -18,13 +19,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  // User.findById(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
-  next();
+  User.findById("6398bf599b6899dc62e809b2")
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+ 
 });
 
 app.use('/admin', adminRoutes);
@@ -33,5 +34,6 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 mongoConnect(() => {
+
   app.listen(3000);
 });
